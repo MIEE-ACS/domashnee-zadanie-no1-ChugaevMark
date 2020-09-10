c1521546989cc9cfd10ea4409ed87159d848a8cc
ChugaevMark <chugaev-mark@inbox.ru>
2020-09-10 14:47:05 +0300

    Задание выполнено

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string tc = Console.ReadLine();
+			if (tc.Length <= 10)
+			{
+				int TC = int.Parse(tc);
+				int TF = TC * 9 / 5 + 32;
+				Console.WriteLine($"{TF}");
+			}
+			else
+			{
+				Console.WriteLine("Ввод неверный");
+			}
+		}
+	}
+}
